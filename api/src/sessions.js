e94e192ae89dbb7b41250c0d14fe449ecdea19af
pequx <maciej@piernikowski.net>
2019-04-30 13:59:03 +0200

    [API-2] adjusted session with error handling

--- api/src/sessions.js
+++ api/src/sessions.js
@@ -9,17 +9,23 @@ const { REDIS_HOST, REDIS_PORT, REDIS_PREFIX, SESSION_SECRET } = process.env;
  * @param {Object|False} container
  */
 module.exports = (container = false) => {
-  const { app, session, RedisStore } = container.cradle;
-  app.use(
-    session({
-      store: new RedisStore({
-        host: REDIS_HOST,
-        port: +REDIS_PORT,
-        prefix: `${REDIS_PREFIX}:sess:`,
-      }),
-      secret: SESSION_SECRET,
-      resave: true,
-      saveUninitialized: true,
-    }),
-  );
+  try {
+    if (this._container instanceof Object) {
+      const { app, session, RedisStore } = container.cradle;
+      app.use(
+        session({
+          store: new RedisStore({
+            host: REDIS_HOST,
+            port: +REDIS_PORT,
+            prefix: `${REDIS_PREFIX}:sess:`,
+          }),
+          secret: SESSION_SECRET,
+          resave: true,
+          saveUninitialized: true,
+        }),
+      );
+    }
+  } catch (error) {
+    console.error('Session handler', error);
+  }
 };
