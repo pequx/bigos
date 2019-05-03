b5ff7c45ca93678c02a01ed1c05d559009a74255
pequx <d335a2ce@pequ.net>
2019-05-03 13:44:32 +0200

    [FRO-1] lint adjustments to current source and infra

--- frontend/scripts/test.js
+++ frontend/scripts/test.js
@@ -13,7 +13,6 @@ process.on('unhandledRejection', err => {
 // Ensure environment variables are read.
 require('../config/env');
 
-const jest = require('jest');
 const argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
